c2c035410c40fb86354d569052957d5a528586a3
[Sasha] <[plakhotnik2003@mail.ru]>
2021-08-03 20:27:04 +0300

    Упражнение 1

---
+++ L1.py
@@ -0,0 +1,38 @@
+sec = int(input('Введите секунды: '))
+days = 0
+hours = 0
+minutes = 0
+seconds = 0
+count_h = 1
+count_m = 1
+answer = ''
+
+if sec // 60 >= 1:
+	if sec // 3600 >= 1:
+		if sec // 86400 >= 1:
+			days = sec // 86400
+			answer = str(days) + ' дн '
+		hours = (sec - days * 86400) // 3600
+		while hours > 59:
+			hours -= 60
+			count_h +=1
+		answer += str(hours) + ' час '
+	minutes = (sec - days * 86400 - hours * count_h * 3600) // 60
+	while minutes > 59:
+			minutes -= 60
+			count_m +=1
+	answer += str(minutes) + ' мин '
+seconds = (sec - days * 86400 - hours * 3600 - minutes * count_m * 60)
+answer += str(seconds) + ' сек'
+print(answer)
+
+	
+		
+
+
+		#hours = sec // 3600
+		#minutes = (sec - hours * 3600) // 60
+		#seconds = (sec - hours * 3600 - minutes * 60)
+		#print(hours)
+		#print(minutes)
+		#print(seconds)
