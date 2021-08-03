0bd10f262867c68d58e5eb5fd065befc012c87e1
SPlakh2201 <88394110+SPlakh2201@users.noreply.github.com>
2021-08-03 20:34:38 +0300

    Update L1.py

--- L1.py
+++ L1.py
@@ -25,14 +25,3 @@ if sec // 60 >= 1:
 seconds = (sec - days * 86400 - hours * 3600 - minutes * count_m * 60)
 answer += str(seconds) + ' сек'
 print(answer)
-
-	
-		
-
-
-		#hours = sec // 3600
-		#minutes = (sec - hours * 3600) // 60
-		#seconds = (sec - hours * 3600 - minutes * 60)
-		#print(hours)
-		#print(minutes)
-		#print(seconds)
